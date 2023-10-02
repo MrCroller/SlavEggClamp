59ad5a85c38cd833b684da0c20bad217571d2598
Mr.Croller <pav199925@gmail.com>
2023-10-02 13:46:27 +0300

    Добавлено меню

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject optionMenu;
+
+    public bool OptionOpen
+    {
+        set
+        {
+            mainMenu.SetActive(!value);
+            optionMenu.SetActive(value);
+        }
+    }
+
+    private void Start()
+    {
+        mainMenu.SetActive(true);
+        optionMenu.SetActive(false);
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
